c11594292ffed175c88ca05cb5a41d8581beee29
DiegoRMV <95139700+DiegoRMV@users.noreply.github.com>
2023-11-30 20:33:09 -0300

    primer commit

---
+++ menu-animado/js/main.js
@@ -0,0 +1,6 @@
+let menuHamburguesa = document.querySelector(".nav__logo");
+
+menuHamburguesa.addEventListener("click", () => {
+	let menu = document.querySelector(".list");
+    menu.classList.toggle("list--show");
+});
